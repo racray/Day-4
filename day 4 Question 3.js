f3e7dfa23caf012bdabc4c303bf31df707aa0fcd
Rayed Khan <60972839+racray@users.noreply.github.com>
2021-09-28 18:49:30 +0530

    Update day 4 Question 3.js

--- day 4 Question 3.js
+++ day 4 Question 3.js
@@ -1,7 +1,6 @@
-///////////////////////// WEBSITE NOT OPENING /////////////////
 var countries = () => {
     var xhr = new XMLHttpRequest;
-    xhr.open("GET","https://restcountries.eu/rest/v2/all");
+    xhr.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
     xhr.responseType = "json";
     xhr.onload = () =>{
         const obj = xhr.response;
@@ -10,11 +9,13 @@ var countries = () => {
             console.log(blog.region);
             console.log(blog.subregion);
             console.log(blog.population);
- 
-        }
+            }
     }
     xhr.send();
- 
 }
- 
 countries();
+
+
+
+
+
